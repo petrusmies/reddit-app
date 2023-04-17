a985fe387a9808d77fe68233493364477de84f29
petrusmies <69676318+petrusmies@users.noreply.github.com>
2023-04-17 14:30:09 +0300

    add working login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Navigate, useRoutes } from 'react-router-dom';
 import './App.css';
+import Login from './routes/Login';
+import Posts from './routes/Posts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  let element = useRoutes([
+    { path: '/login', element: <Login /> },
+
+    // Private routes
+    { path: '/', element: <PrivateRoute element={<Posts />} /> },
+  ]);
+  
+  return element;
 }
+
+// Wrapper component for routes. If no token in localStorage, redirect to login page.
+const PrivateRoute = (props: any) => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+  return props.element;
+};
 
 export default App;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,27 +1,69 @@
 import axios from "axios";
 
-const { REACT_APP_API_URL } = process.env;
+interface IAuth {
+  login: () => void;
+  logout: () => void;
+  auth: () => { code: string } | { error: string } | null;
+  oauth: (code: string) => Promise<any>;
+  setToken: (token: string) => void;
+}
 
-// Login user and store access token in session storage
-const login = (username: string, password: string) => {
-  return axios.post(REACT_APP_API_URL + "/access_token", { username, password })
-  .then((response) => {
-    if (response.data.access_token) {
-      sessionStorage.setItem("user", JSON.stringify(response.data));
+const authService: IAuth = {
+  login: () => {
+    const RANDOM_STRING = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    sessionStorage.setItem('state', RANDOM_STRING);
+    const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI, REACT_APP_RESPONSE_TYPE, REACT_APP_DURATION, REACT_APP_SCOPE } = process.env;
+    window.location.href = `https://www.reddit.com/api/v1/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=${REACT_APP_RESPONSE_TYPE}&state=${RANDOM_STRING}&redirect_uri=${REACT_APP_REDIRECT_URI}&duration=${REACT_APP_DURATION}&scope=${REACT_APP_SCOPE}`
+  },
+  // Remove access token from session storage
+  logout: () => {
+    sessionStorage.removeItem('token');
+  },
+  // Get auth code from reddit
+  auth: () => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+    const state = params.get('state');
+    const error = params.get('error');
+    if (code && state && state === sessionStorage.getItem('state')) {
+      return { code };
+    } else if (error) {
+      return { error };
+    } else {
+      return null;
+    }
+  },
+  // Get access token from reddit
+  oauth: async (code: string) => {
+    const { REACT_APP_REDIRECT_URI, REACT_APP_CLIENT_ID, REACT_APP_SECRET } = process.env;
+    const clientId = REACT_APP_CLIENT_ID ?? '';
+    const clientSecret = REACT_APP_SECRET ?? '';
+    const credentials = `${clientId}:${clientSecret}`;
+    const encodedCredentials = window.btoa(credentials);
+
+    const response = await axios.post('https://www.reddit.com/api/v1/access_token', null, {
+      params: {
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REACT_APP_REDIRECT_URI
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${encodedCredentials}`
+      }
+    }
+    )
+
+    return response.data;
+  },
+  // Set access token to session storage
+  setToken: (token: string) => {
+    if (!token) {
+      throw new Error('Token is required');
     }
     
-    return response.data;
-  })
-}
-
-// Remove access token from session storage
-const logout = () => {
-  sessionStorage.removeItem("user");
-}
-
-const authService = {
-  login,
-  logout
+    sessionStorage.setItem('token', token);
+  }
 }
 
 export default authService;
