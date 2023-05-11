1d1f8d05d38b7441650a6cab5df69e23324131e7
petrusmies <69676318+petrusmies@users.noreply.github.com>
2023-05-11 15:58:44 +0300

    fix bugs in tests, stylize logout button

--- src/components/search/Searchbar.tsx
+++ src/components/search/Searchbar.tsx
@@ -1,33 +1,58 @@
-import { Box, Button, Dialog, DialogTitle, FormControl, Input, InputAdornment, InputLabel, OutlinedInput, TextField, Toolbar } from '@mui/material'
-import { Search } from '@mui/icons-material'
-import React, { Fragment } from 'react'
+import { Logout, Search } from '@mui/icons-material'
+import { Avatar, Box, Button, IconButton, Toolbar } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import React, { Fragment } from 'react'
 import SearchModal from './SearchModal'
 
 // styled button made to look like search field
 const SearchButton = styled(Button)(({ theme }) => ({
-  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  backgroundColor: 'rgba(255, 255, 255, 0.7)',
   color: 'rgba(0, 0, 0, 0.5)',
+  backdropFilter: 'blur(8px)',
   justifyContent: 'flex-start',
+  maxWidth: '600px',
+  width: '100%',
+  gridColumn: 'center',
   '&:hover': {
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
     color: 'rgba(0, 0, 0, 0.5)',
   },
 }))
 
+const LogoutButton = styled(IconButton)(({ theme }) => ({
+  gridColumn: 'right',
+  '& .MuiAvatar-root': {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    transition: 'background-color 0.2s ease-in-out',
+    color: 'rgba(255, 255, 255, 1)',
+
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.5)',
+      color: 'rgba(0, 0, 0, 0.9)',
+    }
+  }
+}))
+
 const Searchbar = () => {
   const [modalOpen, setModalOpen] = React.useState<boolean>(false)
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token')
+    window.location.reload()
+  }
 
   return (
     <Fragment>
       <Box data-testid='searchbar'>
         <Toolbar
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            maxWidth: '600px',
-            margin: '0 auto',
+            display: 'grid',
+            gridTemplateColumns: {
+              xs: '[center] minmax(200px , 600px) [right] minmax(60px , auto)',
+              sm: '[left] minmax(60px , auto) [center] minmax(200px , 600px) [right] minmax(60px , auto)'
+            },
+            justifyContent: 'space-between ',
+            gridGap: '16px',
             py: 4
           }}
         >
@@ -37,9 +62,19 @@ const Searchbar = () => {
             startIcon={<Search />}
             fullWidth
             onClick={() => setModalOpen(true)}
+            aria-label='search'
           >
             Search...
           </SearchButton>
+          <LogoutButton
+            data-testid='logout-button'
+            onClick={() => handleLogout()}
+            aria-label='logout'
+          >
+            <Avatar>
+              <Logout fontSize='medium' />
+            </Avatar>
+          </LogoutButton>
         </Toolbar>
       </Box>
       <SearchModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
